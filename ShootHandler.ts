7c2c0c73ccc4c88530885af2458a822534e22ddb
Melinda Sak <melinda@saksys.de>
2024-05-19 20:02:52 +0200

    bomb images seperated

--- ShootHandler.ts
+++ ShootHandler.ts
@@ -11,44 +11,10 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Stage == "Game") {
         if (ShootDirection == "left") {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . 5 . . . . . . . 
-                . . . . . . . . . . . 5 . . . . 
-                . . . . . . . . d d 2 . . . . . 
-                . . . . . . . d d 5 . . 5 . . . 
-                . . . . . . . d . . . . . . . . 
-                . . . . . f f f f f f . . . . . 
-                . . . . f f f f f f f f . . . . 
-                . . . f f f f f f f f f f . . . 
-                . . . f f f f f f f f f f . . . 
-                . . . f c f f f f f f f f . . . 
-                . . . f c c f f f f f f f . . . 
-                . . . f c c c f f f f f f . . . 
-                . . . f f c c f f c c c f . . . 
-                . . . . f f c c c c f f . . . . 
-                . . . . . f f f f f f . . . . . 
-                `, Hero, -100, 50);
+            projectile = sprites.createProjectileFromSprite(bombLeftIMG, Hero, -100, 50);
             pause(300);
         } else if (ShootDirection == "right") {
-            projectile2 = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 5 . . . . . . . . 
-                . . . . 5 . . . . . . . . . . . 
-                . . . . . 2 d d . . . . . . . . 
-                . . . 5 . . 5 d d . . . . . . . 
-                . . . . . . . . d . . . . . . . 
-                . . . . . f f f f f f . . . . . 
-                . . . . f f f f f f f f . . . . 
-                . . . f f f f f f f f f f . . . 
-                . . . f f f f f f f f f f . . . 
-                . . . f f f f f f f f c f . . . 
-                . . . f f f f f f f c c f . . . 
-                . . . f f f f f f c c c f . . . 
-                . . . f c c c f f c c f f . . . 
-                . . . . f f c c c c f f . . . . 
-                . . . . . f f f f f f . . . . . 
-                `, Hero, 100, 50);
+            projectile2 = sprites.createProjectileFromSprite(bombRightIMG, Hero, 100, 50);
             pause(300);
         }
     }
---
+++ img/bombs.ts
@@ -0,0 +1,39 @@
+const bombLeftIMG = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 5 . . . . . . . 
+. . . . . . . . . . . 5 . . . . 
+. . . . . . . . d d 2 . . . . . 
+. . . . . . . d d 5 . . 5 . . . 
+. . . . . . . d . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f c f f f f f f f f . . . 
+. . . f c c f f f f f f f . . . 
+. . . f c c c f f f f f f . . . 
+. . . f f c c f f c c c f . . . 
+. . . . f f c c c c f f . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+
+
+
+const bombRightIMG = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . 5 . . . . . . . . . . . 
+. . . . . 2 d d . . . . . . . . 
+. . . 5 . . 5 d d . . . . . . . 
+. . . . . . . . d . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f c f . . . 
+. . . f f f f f f f c c f . . . 
+. . . f f f f f f c c c f . . . 
+. . . f c c c f f c c f f . . . 
+. . . . f f c c c c f f . . . . 
+. . . . . f f f f f f . . . . . 
+`;
