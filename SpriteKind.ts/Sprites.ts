43c332b8713d920a18b4d5416639286953e72000
LadyLarak <melinda@saksys.de>
2024-08-09 13:41:15 +0000

    Verfeinerung des Codes 2

---
+++ SpriteKind.ts/Sprites.ts
@@ -0,0 +1,14 @@
+namespace SpriteKind {
+    export const Bad = SpriteKind.create()
+    export const Swarm = SpriteKind.create()
+    export const Raven = SpriteKind.create()
+    export const B1 = SpriteKind.create()
+    export const B2 = SpriteKind.create()
+    export const Dartino1 = SpriteKind.create()
+    export const Fishy = SpriteKind.create()
+    export const Annoying = SpriteKind.create()
+    export const AnnoyingMuch = SpriteKind.create()
+    export const Button = SpriteKind.create()
+    export const S = SpriteKind.create()
+    export const NPC = SpriteKind.create()
+}
--- main.ts
+++ main.ts
@@ -1,19 +1,6 @@
 // lets test an commit
 
-namespace SpriteKind {
-    export const Bad = SpriteKind.create()
-    export const Swarm = SpriteKind.create()
-    export const Raven = SpriteKind.create()
-    export const B1 = SpriteKind.create()
-    export const B2 = SpriteKind.create()
-    export const Dartino1 = SpriteKind.create()
-    export const Fishy = SpriteKind.create()
-    export const Annoying = SpriteKind.create()
-    export const AnnoyingMuch = SpriteKind.create()
-    export const Button = SpriteKind.create()
-    export const S = SpriteKind.create()
-    export const NPC = SpriteKind.create()
-}
+
 namespace StatusBarKind {
     export const Health1 = StatusBarKind.create()
     export const Health2 = StatusBarKind.create()
