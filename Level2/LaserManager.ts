b4e0dcedd9bc8a516cc51846085782f07b552b59
Melinda Sak <melinda@saksys.de>
2024-09-24 22:33:21 +0200

    button animation

--- Level2/LaserManager.ts
+++ Level2/LaserManager.ts
@@ -4,10 +4,59 @@ let RedBtn: Sprite = null
 
 function Laser() {
     RedBtn = sprites.create(redbuttonUp, SpriteKind.colouredButton)
-    RedBtn.x = 0
-    RedBtn.y = 300
+    RedBtn.x = 72
+    RedBtn.y = 315
 
+    sprites.onOverlap(SpriteKind.Player, SpriteKind.colouredButton, function (sprite, otherSprite) {
+        characterAnimations.runFrames(
+            RedBtn,
+            [ img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . f f f f f f f f . . . . 
+    . . . f 2 2 2 1 1 1 1 1 f . . . 
+    f f f 2 2 2 2 2 3 3 3 1 1 f f f 
+    f b f f f f f f f f f f f f 2 f 
+    f c b b b b b 2 2 2 2 2 2 2 2 f 
+    f c c c c c b b b b 2 2 2 2 2 f 
+    f f f f f f f f f f f f f f f f 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . f f f f f f f f . . . . 
+    . . . f 2 2 2 1 1 1 1 1 f . . . 
+    . . f 2 2 2 2 2 3 3 3 1 1 f . . 
+    . f 2 2 2 2 2 2 2 2 2 3 1 2 f . 
+    f f 2 2 2 2 2 2 2 2 2 2 2 2 f f 
+    f b f f f f f f f f f f f f 2 f 
+    f c b b b b b 2 2 2 2 2 2 2 2 f 
+    f c c c c c b b b b 2 2 2 2 2 f 
+    f f f f f f f f f f f f f f f f 
+    `],
+            400,
+            characterAnimations.rule(Predicate.NotMoving)
+        )
 
+        console.log("redbutton")
+        Laser1.x = -100000
+        Laser1.y = -100000
+        Laser2.x = 248
+        Laser2.y = 32
+        
+    })
+    
     Laser1 = sprites.create(laserRedIMG, SpriteKind.Annoying)
     Laser1.x = 296
     Laser1.y = 180
@@ -21,11 +70,7 @@ function Laser() {
         // Laser1 = sprites.create(laserRedIMG, SpriteKind.Annoying)
         // Laser1.x = 296
         // Laser1.y = 180
-        console.log("redbutton")
-        Laser1.x = -100000
-        Laser1.y = -100000
-        Laser2.x = 248
-        Laser2.y = 32
+       
     })
     
     // yellow button
--- images.g.ts
+++ images.g.ts
@@ -37,7 +37,42 @@ fffffffffffffcfffffffffffffff3ffffffffffff3ff
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "redklick":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f 2 2 2 1 1 1 1 1 f . . . 
+. . f 2 2 2 2 2 3 3 3 1 1 f . . 
+. f 2 2 2 2 2 2 2 2 2 3 1 2 f . 
+f f 2 2 2 2 2 2 2 2 2 2 2 2 f f 
+f b f f f f f f f f f f f f 2 f 
+f c b b b b b 2 2 2 2 2 2 2 2 f 
+f c c c c c b b b b 2 2 2 2 2 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f 2 2 2 1 1 1 1 1 f . . . 
+f f f 2 2 2 2 2 3 3 3 1 1 f f f 
+f b f f f f f f f f f f f f 2 f 
+f c b b b b b 2 2 2 2 2 2 2 2 f 
+f c c c c c b b b b 2 2 2 2 2 f 
+f f f f f f f f f f f f f f f f 
+`];
         }
         return null;
     })
